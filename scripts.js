a79fd96bbcee582236d48e44ff900a228abb324d
Kacper Kaczmarzyk <kacper.kaczmarzyk@gmail.com>
2025-02-13 22:33:04 +0100

    update all files to achieve scrolling sections, add resume.html

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-// this is purposefully empty for now
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
