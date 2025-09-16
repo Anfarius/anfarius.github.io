0bec64559d8b5fc90aeaa62e133c76edfafba4b7
Anfarius <kacper.kaczmarzyk@gmail.com>
2025-09-16 19:58:20 +0200

    add partial content to credits page

--- src/data/credits-data.js
+++ src/data/credits-data.js
@@ -1,14 +1,105 @@
 // src/data/credits-data.js
 
+import FBC from '../assets/banners/fbc-firebreak-banner.jpg'
+import AlanWake2 from '../assets/banners/alan-wake-2-banner.jpg'
+import LifeIsStrangeDoubleExposure from '../assets/banners/lis-de-banner.jpg'
+import Immortals from '../assets/banners/immortals-of-aveum-banner.jpg'
+import Outriders from '../assets/banners/outriders-banner.jpg'
+import LisBts from '../assets/banners/lis-bts-banner.jpeg'
+import Lir from '../assets/banners/lir-banner.jpg'
+import Powerwash from '../assets/banners/powerwash-banner.jpg'
+import Realpolitiks from '../assets/banners/realpolitiks2-banner.jpeg'
+import CK3 from '../assets/banners/ck3-banner.jpg'
+
 export const creditData = [
     {
-        id: 1,
+        title: "FBC: Firebreak",
+        year: "2025",
+        developer: "Remedy Entertainment",
+        platforms: "PC, Xbox Series X/S, PS5",
+        role: "FQA Project Manager",
+        image: FBC,
+        storeLink: "https://store.steampowered.com/app/2272540/FBC_Firebreak/"
+    },
+    {
+        title: "Life is Strange: Double Exposure",
+        year: "2024",
+        developer: "Deck Nine",
+        platforms: "PC, Xbox Series X/S, PS5, Nintendo Switch",
+        role: "FQA Project Manager",
+        image: LifeIsStrangeDoubleExposure,
+        storeLink: "https://store.steampowered.com/app/1874000/Life_is_Strange_Double_Exposure/"
+    },
+    {
         title: "Alan Wake 2",
         year: "2023",
         developer: "Remedy Entertainment",
-        platforms: "PC, Xbox Series X, PS5",
-        role: "QA Project Manager",
-        image: "/images/alan-wake-2-banner.jpg",
+        platforms: "PC, Xbox Series X/S, PS5",
+        role: "FQA Project Manager",
+        image: AlanWake2,
         storeLink: "https://store.epicgames.com/en-US/p/alan-wake-2"
-    }
+    },
+    {
+        title: "Immortals of Aveum",
+        year: "2023",
+        developer: "Ascendant Studios",
+        platforms: "PC, Xbox Series X/S, PS5",
+        role: "FQA Project Manager",
+        image: Immortals,
+        storeLink: "https://store.steampowered.com/app/2009100/Immortals_of_Aveum/"
+    },
+    {
+        title: "Outriders: Worldslayer",
+        year: "2022",
+        developer: "People Can Fly",
+        platforms: "PC, Xbox Series X/S, PS5",
+        role: "FQA Project Manager",
+        image: Outriders,
+        storeLink: "https://store.steampowered.com/app/1785250/OUTRIDERS_WORLDSLAYER_EXPANSION/"
+    },
+    {
+        title: "Life is Strange: Before the Storm - Remastered",
+        year: "2022",
+        developer: "Deck Nine",
+        platforms: "PC, Xbox Series X/S, Xbox One, PS5, PS4, Nintendo Switch",
+        role: "FQA Project Manager",
+        image: LisBts,
+        storeLink: "https://store.steampowered.com/app/1265930/Life_is_Strange_Before_the_Storm_Remastered/"
+    },
+    {
+        title: "PowerWash Simulator",
+        year: "2022",
+        developer: "FuturLab",
+        platforms: "PC, Xbox Series X/S, PS5, Nintendo Switch",
+        role: "FQA Project Manager",
+        image: Powerwash,
+        storeLink: "https://store.steampowered.com/app/1290000/PowerWash_Simulator/"
+    },
+    {
+        title: "Lost in Random",
+        year: "2021",
+        developer: "Zoink, Thunderful",
+        platforms: "PC, Xbox Series X/S, Xbox One, PS5, PS4, Nintendo Switch",
+        role: "FQA Project Lead",
+        image: Lir,
+        storeLink: "https://store.steampowered.com/app/1462570/Lost_in_Random/"
+    },
+    {
+        title: "Crusader Kings III",
+        year: "2020",
+        developer: "Paradox Development Studio",
+        platforms: "PC",
+        role: "FQA Project Lead",
+        image: CK3,
+        storeLink: "https://store.steampowered.com/app/1158310/Crusader_Kings_III/"
+    },
+    {
+        title: "Realpolitiks 2",
+        year: "2020",
+        developer: "Jujubee",
+        platforms: "PC",
+        role: "QA Tester",
+        image: Realpolitiks,
+        storeLink: "https://store.steampowered.com/app/1248060/Realpolitiks_II/"
+    },
 ];
