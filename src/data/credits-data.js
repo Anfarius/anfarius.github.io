97fa43c2c529209692ef1e8c599ce1f4f9ab4291
Anfarius <kacper.kaczmarzyk@gmail.com>
2025-09-16 17:52:33 +0200

    remove Astro placeholdr assets and components

---
+++ src/data/credits-data.js
@@ -0,0 +1,14 @@
+// src/data/credits-data.js
+
+export const creditData = [
+    {
+        id: 1,
+        title: "Alan Wake 2",
+        year: "2023",
+        developer: "Remedy Entertainment",
+        platforms: "PC, Xbox Series X, PS5",
+        role: "QA Project Manager",
+        image: "/images/alan-wake-2-banner.jpg",
+        storeLink: "https://store.epicgames.com/en-US/p/alan-wake-2"
+    }
+];
